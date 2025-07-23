a5ef72b0ff8ac127b75968185aa2cc28a648080f
Maksud Alam <maksud.patna@gmail.com>
2025-07-24 00:27:47 +0530

    connect database

--- src/app.js
+++ src/app.js
@@ -1,6 +1,21 @@
 const express = require("express");
 require('./config/data')
 const app = express();
+const user=require('./model/user')
+
+app.post("/singup", async(req , res)=>{
+    const users=new user({
+        firstName :"Maksud",
+        LastName :"Alam",
+        email:"maksud.patna@123",
+       
+
+    });
+    await users.save()
+    res.send("succesfully data  connect!")
+})
+
+
 
 
 app.listen(3000, () => {
--- src/config/data.js
+++ src/config/data.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://maksudalam:maksudxyz@cluster0.9wtuf7x.mongodb.net/devtinder?retryWrites=true&w=majority&appName=Cluster0"
+    "mongodb://localhost:27017/devtinder"
   );
 };
 
