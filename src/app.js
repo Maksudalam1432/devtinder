1945ece84990f0b949480a75e6b30f39c1f2bea4
Maksud Alam <maksud.patna@gmail.com>
2025-07-25 22:36:20 +0530

    implement api get post patch

--- src/app.js
+++ src/app.js
@@ -5,19 +5,112 @@ const user=require('./model/user')
 
 app.use(express.json())
 
+// app.post("/singup", async(req , res)=>{
+  
+//    console.log(req.body)
+//     const users=new user({
+//         firstName :"Washimali   ",
+//         LastName :"Khan",
+//         email:"washimkhan@123", 
+       
+//     });
+//     await users.save()
+//     res.send("succesfully data  connect!")
+// })
 app.post("/singup", async(req , res)=>{
   
    console.log(req.body)
-    const users=new user({
-        firstName :"Maksud",
-        LastName :"Alam",
-        email:"maksud.patna@123", 
-       
+    const { firstName,LastName, email } = req.body;
 
-    });
+    const users = new user({
+     firstName,
+     LastName,
+      email
+      
+    })
     await users.save()
     res.send("succesfully data  connect!")
 })
+
+ // find and findone
+
+app.get("/user",async(req ,res)=>{
+  const userid=req.body.email;
+
+  // try {
+
+  //   const users= await user.findOne({email:userid}).exec();;
+  //   // const users= await user.find({email:userid});
+  //   res.send(users)
+  // }
+  // catch(error){
+  //     res.status(404).send("user not found")
+  // }
+  try {
+
+    const users= await user.find({email:userid});
+    res.send(users)
+  }
+  catch(error){
+      res.status(404).send("user not found")
+  }
+})
+
+// find by id 
+
+
+app.get("/user", async (req, res) => {
+  const userid = req.body.userid;
+
+  try {
+    const users = await user.findById(userid);
+
+    if (!users) {
+      return res.status(404).send("User not found");
+    }
+
+    res.send(users);
+  } catch (error) {
+    res.status(500).send("Server error");
+  }
+});
+
+//findbyidanddeleta
+app.delete("/user", async (req, res) => {
+  const userid = req.body.userid;
+
+  try {
+    const User1 = await user.findByIdAndDelete(userid);
+
+    if (User1) {
+      return res.status(404).send("User not found");
+    }
+
+    res.send("Delete is successful");
+  } catch (error) {
+    res.status(500).send("Something went wrong");
+  }
+});
+
+//update
+
+app.patch("/user", async (req, res) => {
+  const usersid = req.body.userid; // fix 1
+  const data = req.body;
+
+  try {
+    const updatedUser = await user.findByIdAndUpdate({ _id:usersid},data); 
+
+    if (!updatedUser) {
+      return res.status(404).send("User not found");
+    }
+
+    res.send("User updated successfully");
+  } catch (error) {
+    res.status(400).send("Error updating user: " + error.message);
+  }
+});
+
 
 
 
